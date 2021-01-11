34ee0e17d4a5ac0e06dd701f32f8ace62056f51b
PatrickSachs <kontakt@patrick-sachs.de>
2021-01-11 15:43:21 +0100

    Updated script

--- best_socks.rb
+++ best_socks.rb
@@ -1,35 +1,18 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class BestSocks < Formula
   desc "with AppsBox for MacOS"
   homepage "https://www.cordaware.com/eng/bestsocks"
   url "https://download.cordaware.com/best/best_socks/1.1.0/best_socks-macosx.zip"
   sha256 "0c90406b8e5ffa755edf2c5bbe7b6776ab13e640be94a7e6acb496928c3659f6"
   license "Proprietary"
-
-  # depends_on "cmake" => :build
-
+  
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
-  end
+    # Extract app file
+    system "unzip", "./app/best_apps.app.zip"
+    system "rm", "-r", "app"
 
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test best_socks`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    #system "false"
+    # Copy files
+    system "cp", "-R", ".", "#{prefix}"
   end
 end
